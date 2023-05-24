e064745c54388487aa19f0a40629ac2cc8d2c6e9
Liam Mackle <liam.mackle1991@gmail.com>
2023-05-24 14:53:19 +0000

    add js code to randomise card from array

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
 })
 
-var suits = ['H','C','D','S'];
-var cards = ['2','3','4','5','6', '7', '8', '9', '10', 'J', 'Q','K','A' ];
-var deck = [];
-for(var suitCounter = 0; suitCounter < 4; suitCounter++) {  
-    for(var cardsCounter = 0; cardsCounter < 13; cardsCounter++) {
-        deck.push(cards[cardsCounter]) + suits[suitCounter];
-    }
-}
+var cards = ['H2','H3','H4','H5','H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ','HK','HA','C2','C3','C4','C5','C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ','CK','CA','D2','D3','D4','D5','D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ','DK','DA','S2','S3','S4','S5','S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ','SK','SA' ];
 
 const currentCard = document.getElementById("previousCard");
 const nextCard = document.getElementById("nextCard");
 const buttons = document.getElementsByClassName("btn");
 const score = document.getElementById("score")
 
-let next_card = deck[Math.floor(Math.random() * deck.length)];
+let current_card = cards[Math.floor(Math.random() * cards.length)];
+let next_card = cards[Math.floor(Math.random() * cards.length)];
 let streak = 0;
-console.log(next_card);
 
+previousCard.innerText = current_card;
 nextCard.innerText = next_card;
+
+
 
 
