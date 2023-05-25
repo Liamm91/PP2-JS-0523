9f881f7ca6fef671321ce1f718e88720fbebfbdb
Liam Mackle <liam.mackle1991@gmail.com>
2023-05-25 17:35:37 +0000

    Add JS to randomise cards dealt and store the previous card

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,70 @@ document.addEventListener("DOMContentLoaded", function() {
 
 })
 
-var cards = ['H2','H3','H4','H5','H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ','HK','HA','C2','C3','C4','C5','C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ','CK','CA','D2','D3','D4','D5','D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ','DK','DA','S2','S3','S4','S5','S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ','SK','SA' ];
+var suits = ['H','D','C','S']
+var cards = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
+var deck = [];
 
-const currentCard = document.getElementById("previousCard");
+for (var suitCounter = 0; suitCounter < 4; suitCounter++) {
+    for (var cardCounter = 0; cardCounter < 13; cardCounter++) {
+        deck.push(cards[cardCounter] + suits[suitCounter]);
+    }
+}
+
+const lastCard = document.getElementById("lastCard")
 const nextCard = document.getElementById("nextCard");
 const buttons = document.getElementsByClassName("btn");
 const score = document.getElementById("score")
+const result = document.getElementById("result")
 
-let current_card = cards[Math.floor(Math.random() * cards.length)];
-let next_card = cards[Math.floor(Math.random() * cards.length)];
+let last_card = 0;
+let next_card = deck[Math.floor(Math.random() * deck.length)];
 let streak = 0;
 
-previousCard.innerText = current_card;
 nextCard.innerText = next_card;
+lastCard.innerText = last_card;
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        StartGame(this.dataset.value);
+    })
+}
+
+function StartGame (choice) {
+
+    last_card = next_card;
+    next_card = deck[Math.floor(Math.random() * deck.length)];
+    lastCard.innerText = last_card;
+    nextCard.innerText = next_card;
+
+    let compare;
+
+    if (last_card < next_card) {
+        compare = "higher";
+    } else if (last_card > current_card) {
+        compare = "lower";
+    } else  {
+        compare = "pair";
+    }
+    
+    if (choice == compare) {
+        score++;
+        score.innerText = score;
+        result.innerText = "Correct";
+        result.classList.remove("correct");
+        result.classList.remove("clear");
+    } else if (compare == "pair") {
+        result.innerText = "You don't get anything for a pair.....Not in this game";
+        result.classList.remove("clear");
+    } else {
+        result.innerText = "Incorrect";
+        result.classList.add("incorrect")
+        result.classList.remove("clear");
+
+        score = 0;
+    }
+
+}
 
 
 
